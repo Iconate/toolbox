582d55533be7c66906e711a378fc2311ef8a0d12
Nathan Krueger <nathan.krueger@blueprintagencies.com>
2014-08-19 16:59:17 -0400

    Progress through s1c3

--- s1c3.rb
+++ s1c3.rb
@@ -12,50 +12,39 @@ How? Devise some method for "scoring" a piece of English plaintext. Character fr
 
 #nice visual: "1b 37 37 33 31 36 3f 78 15 1b 7f 2b 78 34 31 33 3d 78 39 78 28 37 2d 36 3c 78 37 3e 78 3a 39 3b 37 36"
 hexstring = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+#answer =   "436f6f6b696e67204d432773206c696b65206120706f756e64206f66206261636f6e"
 
-# at least I believe thats the answer
-answer =    "436f6f6b696e67204d432773206c696b65206120706f756e64206f66206261636f6e"
-
-def hex_char_frequency(hex_string)
-  frequencies = {
-    1 => 0,
-    2 => 0,
-    3 => 0,
-    4 => 0,
-    5 => 0,
-    6 => 0,
-    7 => 0,
-    8 => 0,
-    9 => 0,
-    10 => 0,
-    11 => 0,
-    12 => 0,
-    13 => 0,
-    14 => 0,
-    15 => 0,
-    16 => 0
-  }
-  hex_string.each_char do |x|
-    index = x.to_i(16)
-    current_val = frequencies[index]
-    frequencies[index] = current_val + 1
-  end
-  puts frequencies
+def bin_to_hex(s)
+  s.each_byte.map { |b| b.to_s(16) }.join
 end
 
-def readable_xor(hexstring, character)
-  require_relative "s1c2"
-  return [xor(hexstring, character)].pack("H*")
+def hex_to_bin(s)
+  s.scan(/../).map { |x| x.hex }.pack('c*')
 end
 
-def exhaustive_hex_xor(hexstring)
-  for hex_val in 0..255 do
-    puts readable_xor(hexstring, "#{hex_val.to_s(16)}")
+def repeated_char_xor(hex_string, hex_char)
+  repeated_char = hex_char * (hex_string.length/hex_char.length)
+  return xor(hex_string, repeated_char)
+end
+
+def freq_analysis(some_string)
+  #initialize  character array
+  char_arr =  some_string.split(//).uniq
+  hash = {}
+  char_arr.each do |x|
+    hash[x] = 0
   end
+
+  #count occurances
+  return hash
 end
 
 require_relative "s1c2"
-puts xor(hexstring, "FFFFFFFFFFFF")
+require_relative "s1c1"
+result = repeated_char_xor(hexstring,bin_to_hex("C"))
+almost= hex_to_bin(result)
+puts freq_analysis(almost)
+
 
 
 
